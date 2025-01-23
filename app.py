4cdd93de107433a88c52ced34d218c1ec70db6f3
Godwin <godwinyampoi449@gmail.com>
2025-01-23 16:35:57 +0300

    made the staff list

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask , render_template, request , url_for,redirect
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from models import db,Projects
+from models import Staff
 
 
 
@@ -42,7 +43,7 @@ def delete_project(id):
     project_to_delete = Projects.query.get(id)
     if project_to_delete:
         db.session.delete(project_to_delete)
-        db.session.commit() 
+        db.session.commit() #After modifying data, you must call db.session.commit() to commit the changes to the database. Otherwise, they will be discarded at the end of the request.
     return redirect(url_for('projects'))
 
 @app.route('/projects/update/<int:id>', methods=['POST'])
@@ -55,6 +56,23 @@ def update_project(id):
         project_to_update.description = project_details
         db.session.commit()
     return redirect(url_for('projects'))
+
+@app.route('/staff', methods=['GET','POST'])
+def staff():
+     if request.method == 'POST':
+       first_name = request.form.get('first_name')
+       last_name = request.form.get('last_name')
+       profile_photo = request.form.get('profile_photo')
+
+       new_staff = Staff(first_name=first_name,last_name=last_name,profile_photo=profile_photo)
+       db.session.add(new_staff)
+       db.session.commit()
+
+       return redirect(url_for('staff'))
+     
+     all_staff = Staff.query.all()
+     return render_template('staff.html', staff=all_staff)
+
 
 
 
